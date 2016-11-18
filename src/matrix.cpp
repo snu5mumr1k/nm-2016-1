cb98cf4c1395fd5c8ed88507d144b838e1547b01
Andre Feofilaktov <andarpolis@gmail.com>
2016-11-18 18:23:25 +0300

    matrix class improved

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -23,8 +23,34 @@ Matrix::Matrix(int n, const double *elements) :
     memcpy(elements_, elements, n * n * sizeof *elements_);
 }
 
+Matrix::Matrix(int n, filling_function get_element) :
+    Matrix(n)
+{
+    elements_ = new double[n * n];
+
+    for (int i = 0; i < n; ++i) {
+        elements_[n * i + j] = get_element(i, j);
+    }
+}
+
+Matrix::Matrix(const Matrix &other) :
+    Matrix(other.n_, other.elements_)
+{}
+
 Matrix::~Matrix() {
     delete [] elements_;
+}
+
+Matrix &Matrix::operator =(const Matrix &other) {
+    if (this != &other) {
+        delete [] elements_;
+
+        n_ = other.n_;
+        elements_ = new double[n_ * n_];
+        memcpy(elements_, other.elements_, n_ * n_ * sizeof *elements_);
+    }
+
+    return *this;
 }
 
 void Matrix::Print() const {
@@ -37,8 +63,12 @@ void Matrix::Print() const {
     std::cout << std::endl;
 }
 
+int Matrix::Size() const {
+    return n_;
+}
+
 void Matrix::SwapRows(int i, int j) {
-    if (OutOfBounds(i) || OutOfBounds(j)) {
+    if (OutOfRange(i) || OutOfRange(j)) {
         throw std::runtime_error("Matrix index is out of bounds");
     }
     if (i == j) {
@@ -54,37 +84,60 @@ void Matrix::SwapRows(int i, int j) {
     delete [] tmp;
 }
 
+void Matrix::MultiplyRow(int i, double multiplier) {
+    for (int j = 0; j < n_; ++j) {
+        elements_[i * n_ + j] *= multiplier;
+    }
+}
+
 void Matrix::AddRow(int dest, int src, double multiplier) {
     for (int i = 0; i < n_; ++i) {
         elements_[dest * n_ + i] += multiplier * elements_[src * n_ + i];
     }
 }
 
-double Matrix::Determinant() const
-{}
+double Matrix::Determinant() {
+    Vector tmp(n_);
+    return GaussianElimination(tmp);
+}
 
-Matrix Matrix::Inverse() const
-{}
+Vector Matrix::Solve(Vector v) {
+    GaussianElimination(v);
+
+    return v;
+}
+
+Matrix Matrix::Inverse() {
+    Matrix result(n_);
+
+    GaussianElimination(result);
+
+    return result;
+}
 
 const double *Matrix::operator [](int i) const {
-    if (OutOfBounds(i)) {
+    if (OutOfRange(i)) {
         throw std::runtime_error("Matrix index is out of range");
     }
 
     return &elements_[i * n_];
 }
 
-Vector Matrix::GaussianElimination(Vector v) {
-    if (v.Size() != n_) {
-        throw std::runtime_error("Matrix and vector sizes dont match");
+template <typename T>
+double Matrix::GaussianElimination(T &right_part) {
+    if (right_part.Size() != n_) {
+        throw std::runtime_error("Parts sizes dont match");
     }
 
-    Vector result(n_);
-
+    double determinant = 1.0;
     for (int i = 0; i < n_; ++i) {
         for (int j = i; j < n_; ++j) {
             if (!Zero(elements_[j * n_ + i])) {
                 SwapRows(i, j);
+                right_part.SwapRows(i, j);
+                if (i != j) {
+                    determinant = -determinant;
+                }
                 break;
             }
         }
@@ -93,23 +146,23 @@ Vector Matrix::GaussianElimination(Vector v) {
             throw std::runtime_error("Matrix is singular");
         }
 
-        for (int j = i + 1; j < n_; ++j) {
-            double coef = -elements_[j * n_ + i] / elements_[i * n_ + i];
+        double coef = 1.0 / elements_[i * n_ + i];
+        MultiplyRow(i, coef);
+        right_part.MultiplyRow(i, coef);
+        determinant /= coef;
+        for (int j = 0; j < n_; ++j) {
+            if (i == j) {
+                continue;
+            }
+            double coef = -elements_[j * n_ + i];
             AddRow(j, i, coef);
-            v[j] += v[i] * coef;
+            right_part.AddRow(j, i, coef);
         }
     }
 
-    for (int i = n_ - 1; i >= 0; --i) {
-        result[i] = v[i];
-        for (int j = i + 1; j < n_; ++j) {
-            result[i] -= result[j] * elements_[i * n_ + j];
-        }
-        result[i] /= elements_[i * n_ + i];
-    }
-    return result;
+    return determinant;
 }
 
-bool Matrix::OutOfBounds(int i) const {
+bool Matrix::OutOfRange(int i) const {
     return i < 0 || i >= n_;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -2,27 +2,37 @@
 
 #include <vector.h>
 
+typedef double (*filling_function)(int i, int j);
+
 class Matrix {
 public:
     Matrix(int n);
     Matrix(int n, const double *elements);
+    Matrix(int n, filling_function get_element);
+    Matrix(const Matrix &other);
 
     ~Matrix();
 
+    Matrix &operator =(const Matrix &other);
+
     void Print() const;
+    int Size() const;
 
     void SwapRows(int i, int j);
+    void MultiplyRow(int i, double multiplier);
     void AddRow(int dest, int src, double multiplier);
 
-    double Determinant() const;
-    Matrix Inverse() const;
-
-    Vector GaussianElimination(Vector v);
+    double Determinant();
+    Vector Solve(Vector v);
+    Matrix Inverse();
 
     const double *operator [](int i) const;
-    bool OutOfBounds(int i) const;
+    bool OutOfRange(int i) const;
 
 private:
     int n_;
     double *elements_;
+
+    template <typename T>
+    double GaussianElimination(T &other);
 };
